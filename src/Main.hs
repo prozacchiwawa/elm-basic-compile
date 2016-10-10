687a26811072797fca933610dad34686feedd56e
prozacchiwawa <art.yerkes@gmail.com>
2016-10-10 03:35:46 -0700

    hfmt, some comments

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,20 @@
 {-# OPTIONS_GHC -Wall #-}
+
 module Main where
 
-import qualified Data.List as List
-import qualified Data.Map as Map
+import           Control.Monad       (mapM)
+import qualified Data.List           as List
+import qualified Data.Map            as Map
 import qualified Data.Text.Lazy
-import Control.Monad (mapM)
 
-import Elm.Compiler
-import Elm.Package
-import Elm.Compiler.Module
+import           Elm.Compiler
+import           Elm.Compiler.Module
+import           Elm.Package
 
 import qualified Utils.File
---import AST.Declaration
---import AST.Variable
---import AST.Module
---import AST.Module.Name
---import AST.Type
 
+-- import AST.Declaration import AST.Variable import AST.Module import AST.Module.Name import
+-- AST.Type
 {-
 
 In order to run the compiler we need several pieces of data:
@@ -82,36 +80,74 @@ data Context = Context {
 compile :: Context -> String -> Elm.Package.Interfaces -> *stuff* :-)
 
 -}
-
 main :: IO ()
 main =
-    -- Grab the package version so we can lookup the built modules according to compiler version
-    let (Version major minor patch) = Elm.Compiler.version in
-    let versionString = List.intercalate "." (map show [major,minor,patch]) in
-    -- Some source code to compile for now
-    let source = "module Test exposing (..)\n\nx = 3" in
-    -- The repository that elm-lang lives in
-    let elmCore = Name { user = "elm-lang", project = "core" } in
-    -- Modules matched to the default imports
-    let importModules = [["Basics"], ["Debug"], ["List"], ["Maybe"], ["Result"], ["Platform"], ["Platform", "Cmd"], ["Platform", "Sub"]] in
-    -- Make a list of the names of modules we need to import
-    let canonicalNames = map (\n -> Elm.Compiler.Module.Canonical elmCore n) importModules in
-    -- A compiler context indicating that we need to import at least the default modules
-    let context = Context { _packageName = Name { user = "elm-lang", project = "test" }, _isExposed = False, _dependencies = canonicalNames } in
-    -- A function to make the hyphenated version of a package name as in build-artifacts
-    let hyphenate rawName = List.intercalate "-" rawName in
-    let fileName (Elm.Compiler.Module.Canonical (Name user project) modPath) = List.intercalate "/" ["elm-stuff", "build-artifacts", versionString, user, project, "4.0.0", (hyphenate modPath) ++ ".elmi"] in
-    let readInterface name = do
-            filename <- pure $ fileName name
-            interface <- Utils.File.readBinary filename
-            return (name, interface)
-    in
-    do
-        interfaces <- mapM readInterface canonicalNames
-        interfaceMap <- pure $ Map.fromList interfaces
-        (localizer, warnings, result) <- pure (Elm.Compiler.compile context source interfaceMap)
-        case result of
-            Left e ->
-                putStrLn ((List.intercalate "\n" (map (Elm.Compiler.errorToString localizer "" source) e)) ++ "\n")
-            Right (Result docs interface js) ->
-                putStrLn (Data.Text.Lazy.unpack js)
+  -- Grab the package version so we can lookup the built modules according to compiler version
+  let (Version major minor patch) = Elm.Compiler.version
+  in let versionString = List.intercalate "." (map show [major, minor, patch])
+     in
+     -- Some source code to compile for now
+     let source = "module Test exposing (..)\n\nx = 3"
+     in
+     -- The repository that elm-lang lives in
+     let elmCore = Name { user = "elm-lang", project = "core" }
+     in
+     -- Modules matched to the default imports
+     let importModules = [ ["Basics"]
+                         , ["Debug"]
+                         , ["List"]
+                         , ["Maybe"]
+                         , ["Result"]
+                         , ["Platform"]
+                         , ["Platform", "Cmd"]
+                         , ["Platform", "Sub"]
+                         ]
+     in
+     -- Make a list of the names of modules we need to import
+     let canonicalNames = map (\n -> Elm.Compiler.Module.Canonical elmCore n) importModules
+     in
+     -- A compiler context indicating that we need to import at least the default modules
+     let context = Context
+                     { _packageName = Name { user = "elm-lang", project = "test" }
+                     , _isExposed = False
+                     , _dependencies = canonicalNames
+                     }
+     in
+     -- A function to make the hyphenated version of a package name as in build-artifacts
+     let hyphenate rawName = List.intercalate "-" rawName
+     in
+     -- A function that builds the relative file name in elm-stuff of an elmi file. We cheat on
+     -- the version number.
+     let fileName (Elm.Compiler.Module.Canonical (Name user project) modPath) = List.intercalate "/"
+              [ "elm-stuff"
+              , "build-artifacts"
+              , versionString
+              , user
+              , project
+              , "4.0.0"
+              , (hyphenate
+                   modPath) ++ ".elmi"
+              ]
+     in
+     -- A function to yield a pair of canonical name and binary interface from a canonical module
+     -- name
+     let readInterface name = do
+         filename <- pure $ fileName name
+         interface <- Utils.File.readBinary filename
+         return (name, interface)
+     in
+     do
+       -- Load up the interfaces into an IO [(Canonical, Interface)]
+       interfaces <- mapM readInterface canonicalNames
+       -- Make the interface map that the compiler consumes
+       interfaceMap <- pure $ Map.fromList interfaces
+       -- Attempt compilation
+       (localizer, warnings, result) <- pure (Elm.Compiler.compile context source interfaceMap)
+       case result of
+         Left e ->
+           -- Failure, print the errors
+           putStrLn
+             ((List.intercalate "\n" (map (Elm.Compiler.errorToString localizer "" source) e)) ++ "\n")
+         Right (Result docs interface js) ->
+           -- Success, print the javascript only for the requested module
+           putStrLn (Data.Text.Lazy.unpack js)
--- src/Utils/File.hs
+++ src/Utils/File.hs
@@ -1,41 +1,46 @@
 -- From elm-make/src/Utils/File.hs
 module Utils.File where
 
-import Control.Monad.Except (liftIO)
+import           Control.Monad.Except (liftIO)
+import qualified Data.Binary          as Binary
 import qualified Data.ByteString.Lazy as LBS
-import qualified Data.Binary as Binary
-import qualified Data.Text as Text
-import qualified Data.Text.IO as TextIO
-import GHC.IO.Exception ( IOErrorType(InvalidArgument) )
-import System.Directory (createDirectoryIfMissing, doesFileExist)
-import System.FilePath (dropFileName)
-import System.IO (utf8, hSetEncoding, withBinaryFile, withFile, Handle, IOMode(ReadMode, WriteMode))
-import System.IO.Error (ioeGetErrorType, annotateIOError, modifyIOError)
+import qualified Data.Text            as Text
+import qualified Data.Text.IO         as TextIO
+import           GHC.IO.Exception     (IOErrorType (InvalidArgument))
+import           System.Directory     (createDirectoryIfMissing, doesFileExist)
+import           System.FilePath      (dropFileName)
+import           System.IO            (Handle, IOMode (ReadMode, WriteMode),
+                                       hSetEncoding, utf8, withBinaryFile,
+                                       withFile)
+import           System.IO.Error      (annotateIOError, ioeGetErrorType,
+                                       modifyIOError)
 
 -- import qualified BuildManager as BM
-
 writeBinary :: (Binary.Binary a) => FilePath -> a -> IO ()
 writeBinary path value =
-  do  let dir = dropFileName path
-      createDirectoryIfMissing True dir
-      withBinaryFile path WriteMode $ \handle ->
-          LBS.hPut handle (Binary.encode value)
-
+  do
+    let dir = dropFileName path
+    createDirectoryIfMissing True dir
+    withBinaryFile path WriteMode $ \handle -> LBS.hPut handle (Binary.encode value)
 
 readBinary :: (Binary.Binary a) => FilePath -> IO a
 readBinary path =
-  do  exists <- liftIO (doesFileExist path)
-      if exists then decode else ioError (userError path)
+  do
+    exists <- liftIO (doesFileExist path)
+    if exists
+      then decode
+      else ioError (userError path)
+
   where
     decode =
-      do  bits <- liftIO (LBS.readFile path)
-          case Binary.decodeOrFail bits of
-            Left _ ->
-                ioError (userError path)
+      do
+        bits <- liftIO (LBS.readFile path)
+        case Binary.decodeOrFail bits of
+          Left _ ->
+            ioError (userError path)
 
-            Right (_, _, value) ->
-                return value
-
+          Right (_, _, value) ->
+            return value
 
 {-|
   readStringUtf8 converts Text to String instead of reading
@@ -48,34 +53,28 @@ readStringUtf8 :: FilePath -> IO String
 readStringUtf8 name =
   readTextUtf8 name >>= (return . Text.unpack)
 
-
 readTextUtf8 :: FilePath -> IO Text.Text
 readTextUtf8 name =
   let action handle =
-        modifyIOError (convertUtf8Error name) (TextIO.hGetContents handle)
-  in
-    withFileUtf8 name ReadMode action
-
+                       modifyIOError (convertUtf8Error name) (TextIO.hGetContents handle)
+  in withFileUtf8 name ReadMode action
 
 convertUtf8Error :: FilePath -> IOError -> IOError
 convertUtf8Error filepath e =
   case ioeGetErrorType e of
     InvalidArgument -> utf8Error
-    _ -> e
+    _               -> e
   where
     errorMessage = "Bad encoding; the file must be valid UTF-8"
     utf8Error = annotateIOError (userError errorMessage) "" Nothing (Just filepath)
-
 
 writeStringUtf8 :: FilePath -> String -> IO ()
 writeStringUtf8 f str =
   writeTextUtf8 f (Text.pack str)
 
-
 writeTextUtf8 :: FilePath -> Text.Text -> IO ()
 writeTextUtf8 f txt =
   withFileUtf8 f WriteMode (\handle -> TextIO.hPutStr handle txt)
-
 
 withFileUtf8 :: FilePath -> IOMode -> (Handle -> IO a) -> IO a
 withFileUtf8 f mode action =
